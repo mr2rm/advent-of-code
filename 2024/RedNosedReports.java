676f8a9d3febd495328675dcbe29e53762b53fc3
MRez <mrez.rahmani@gmail.com>
2024-12-03 00:33:54 +0100

    chore(2024): :white_check:mark: add solution for Red-Nosed Reports - Part 2

--- 2024/RedNosedReports.java
+++ 2024/RedNosedReports.java
@@ -28,11 +28,67 @@ class RedNosedReports {
     }
 
     /**
+     * @param report Report
+     * @param index Index to remove
+     * @return Whether report is safe without index
+     */
+    static boolean isSafeWithout(List<Integer> report, int index) {
+        List<Integer> newReport = new ArrayList<>(report);
+        newReport.remove(index);
+        return isSafe(newReport);
+    }
+
+    /**
+     * @param report Report
+     * @return Whether report is safe with tolerance of 1
+     */
+    static boolean isSafeWithTolerance(List<Integer> report) {
+        if (isSafeWithout(report, 0)) {
+            return true;
+        }
+
+        for (int i = 0; i < report.size() - 1; i++) {
+            int diff = report.get(i + 1) - report.get(i);
+            if (i + 2 < report.size()) {
+                int nextDiff = report.get(i + 2) - report.get(i + 1);
+                if ((diff > 0) != (nextDiff > 0)) {
+                    return isSafeWithout(report, i) || isSafeWithout(report, i + 1)
+                            || isSafeWithout(report, i + 2);
+                }
+            }
+
+            int absDiff = Math.abs(diff);
+            if (absDiff < 1 || absDiff > 3) {
+                return isSafeWithout(report, i) || isSafeWithout(report, i + 1);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Part 2: How many reports are safe with tolerance of 1?
+     *
+     * Brute Force - Time: O(n*m), Space: O(1)
+     * 
+     * @param reports List of reports
+     * @return Number of safe reports with tolerance of 1
+     */
+    static int countSafeReportsWithTolerance(List<List<Integer>> reports) {
+        int safeCount = 0;
+        for (List<Integer> report : reports) {
+            if (isSafeWithTolerance(report)) {
+                safeCount++;
+            }
+        }
+        return safeCount;
+    }
+
+    /**
      * Part 1: How many reports are safe?
      *
      * Brute Force - Time: O(n*m), Space: O(1)
      * 
-     * @param reports List of report
+     * @param reports List of reports
      * @return Number of safe reports
      */
     static int countSafeReports(List<List<Integer>> reports) {
@@ -58,6 +114,8 @@ class RedNosedReports {
         scanner.close();
 
         int safeCount = countSafeReports(reports);
-        System.out.println(safeCount);
+        int safeCountWithTolerance = countSafeReportsWithTolerance(reports);
+        System.out.println("Safe Count: %d\nSafe Count with Tolerance: %d".formatted(safeCount,
+                safeCountWithTolerance));
     }
 }
