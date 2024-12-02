35a60307aef3f17f875be83079c69e305bee35b3
MRez <mrez.rahmani@gmail.com>
2024-12-02 21:42:50 +0100

    refactor(2024): :recycle: use ArrayList instead of array of integers in Red-Nosed Reports

--- 2024/RedNosedReports.java
+++ 2024/RedNosedReports.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 class RedNosedReports {
     /**
@@ -10,14 +11,14 @@ class RedNosedReports {
      * @param report Report
      * @return Whether report is safe or not
      */
-    static boolean isSafe(int[] report) {
-        if (report.length <= 1) {
+    static boolean isSafe(List<Integer> report) {
+        if (report.size() <= 1) {
             return true;
         }
 
-        boolean isIncreasing = report[1] > report[0];
-        for (int i = 1; i < report.length; i++) {
-            int diff = report[i] - report[i - 1];
+        boolean isIncreasing = report.get(1) > report.get(0);
+        for (int i = 1; i < report.size(); i++) {
+            int diff = report.get(i) - report.get(i - 1);
             if (diff == 0 || Math.abs(diff) > 3 || (diff > 0) != isIncreasing) {
                 return false;
             }
@@ -26,16 +27,16 @@ class RedNosedReports {
     }
 
     /**
-     * How many reports are safe?
+     * Part 1: How many reports are safe?
      *
      * Brute Force - Time: O(n*m), Space: O(1)
      * 
      * @param reports List of report
      * @return Number of safe reports
      */
-    static int countSafeReports(List<int[]> reports) {
+    static int countSafeReports(List<List<Integer>> reports) {
         int safeCount = 0;
-        for (int[] report : reports) {
+        for (List<Integer> report : reports) {
             if (isSafe(report)) {
                 safeCount++;
             }
@@ -46,10 +47,11 @@ class RedNosedReports {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
 
-        List<int[]> reports = new ArrayList<>();
+        List<List<Integer>> reports = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            int[] report = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            List<Integer> report = Arrays.stream(line.split(" ")).map(Integer::valueOf)
+                    .collect(Collectors.toList());
             reports.add(report);
         }
         scanner.close();
