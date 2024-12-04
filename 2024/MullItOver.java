ee6be2493c2fbb0807d351e168836a88cf0b8a0c
MRez <mrez.rahmani@gmail.com>
2024-12-04 01:44:13 +0100

    chore(2024): :white_check_mark: solve Mull It Over - Part 2

--- 2024/MullItOver.java
+++ 2024/MullItOver.java
@@ -5,15 +5,40 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class MullItOver {
-    static long multiply(String s) {
+    static int multiply(String s) {
         Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
         Matcher matcher = pattern.matcher(s);
 
-        long result = 0;
+        int result = 0;
         while (matcher.find()) {
-            long a = Long.parseLong(matcher.group(1));
-            long b = Long.parseLong(matcher.group(2));
+            int a = Integer.parseInt(matcher.group(1));
+            int b = Integer.parseInt(matcher.group(2));
             result += a * b;
+        }
+        return result;
+    }
+
+    static int multiplyEnabledInstructions(String s) {
+        Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+        Matcher matcher = pattern.matcher(s);
+
+        int result = 0;
+        boolean enabled = true;
+        while (matcher.find()) {
+            String firstPart = s.substring(0, matcher.start());
+            int lastDoIndex = firstPart.lastIndexOf("do()");
+            int lastDontIndex = firstPart.lastIndexOf("don't()");
+            if (lastDoIndex > lastDontIndex) {
+                enabled = true;
+            } else if (lastDontIndex > lastDoIndex) {
+                enabled = false;
+            }
+
+            if (enabled) {
+                int a = Integer.parseInt(matcher.group(1));
+                int b = Integer.parseInt(matcher.group(2));
+                result += a * b;
+            }
         }
         return result;
     }
@@ -21,14 +46,19 @@ class MullItOver {
     public static void main(String[] args) throws IOException {
         File input = new File("input.in");
         final Scanner scanner = new Scanner(input);
+        StringBuilder builder = new StringBuilder();
 
-        long result = 0;
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            result += multiply(line);
+            builder.append(line);
         }
         scanner.close();
 
-        System.out.println(result);
+        String memory = builder.toString();
+        int multiplyResult = multiply(memory);
+        int enabledMultiplyResult = multiplyEnabledInstructions(memory);
+
+        System.out.println("Multiply Result: %d\nEnabled Multiply Result: %d"
+                .formatted(multiplyResult, enabledMultiplyResult));
     }
 }
