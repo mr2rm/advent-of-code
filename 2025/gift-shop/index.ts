cf8ce44d9b428a2e77666432035deb684389b8d5
mr2rm <mrez.rahmani@gmail.com>
2025-12-05 15:22:58 +0100

    feat(2025): :zap: add another approach for Gift Shop - Part 1

--- 2025/gift-shop/index.ts
+++ 2025/gift-shop/index.ts
@@ -2,9 +2,14 @@ import fs from "fs/promises";
 
 function isInvalidId(id: number) {
   const stringId = id.toString();
-  const left = stringId.slice(0, stringId.length / 2);
-  const right = stringId.slice(stringId.length / 2);
-  return left === right;
+
+  // NOTE: Approach 1: Check for adjacent identical digits
+  // const left = stringId.slice(0, stringId.length / 2);
+  // const right = stringId.slice(stringId.length / 2);
+  // return left === right;
+
+  // NOTE: Approach 2: Regex
+  return /^(\d+)\1$/.test(stringId);
 }
 
 function partOne(ranges: string[]) {
