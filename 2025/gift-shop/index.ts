5dda6b35670a3860e2588301ad013062b0b3c9f6
mr2rm <mrez.rahmani@gmail.com>
2025-12-05 15:39:09 +0100

    feat(2025): :white_check_mark: solve Gift Shop - Part 2

--- 2025/gift-shop/index.ts
+++ 2025/gift-shop/index.ts
@@ -1,18 +1,6 @@
 import fs from "fs/promises";
 
-function isInvalidId(id: number) {
-  const stringId = id.toString();
-
-  // NOTE: Approach 1: Check for adjacent identical digits
-  // const left = stringId.slice(0, stringId.length / 2);
-  // const right = stringId.slice(stringId.length / 2);
-  // return left === right;
-
-  // NOTE: Approach 2: Regex
-  return /^(\d+)\1$/.test(stringId);
-}
-
-function partOne(ranges: string[]) {
+function solve(ranges: string[], isInvalidId: (id: number) => boolean) {
   const invalidIds: number[] = [];
   ranges.forEach((range) => {
     const [start, end] = range.split("-").map(Number);
@@ -25,5 +13,29 @@ function partOne(ranges: string[]) {
   console.log(invalidIds.reduce((sum, id) => sum + id));
 }
 
+function partOne(ranges: string[]) {
+  let isInvalidId = (id: number) => {
+    const stringId = id.toString();
+
+    // NOTE: Approach 1: Check for adjacent identical digits
+    // const left = stringId.slice(0, stringId.length / 2);
+    // const right = stringId.slice(stringId.length / 2);
+    // return left === right;
+
+    // NOTE: Approach 2: Regex
+    return /^(\d+)\1$/.test(stringId);
+  };
+  solve(ranges, isInvalidId);
+}
+
+function partTwo(ranges: string[]) {
+  let isInvalidId = (id: number) => {
+    return /^(\d+)\1+$/.test(id.toString());
+  };
+  solve(ranges, isInvalidId);
+}
+
 const lines = await fs.readFile("./input.txt", "utf-8");
-partOne(lines.split(","));
+let ranges: string[] = lines.split(",");
+// partOne(ranges);
+partTwo(ranges);
