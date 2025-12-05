04fc0ebd9e010dce1a1de1877d64685ab4b102af
mr2rm <mrez.rahmani@gmail.com>
2025-12-05 15:18:07 +0100

    feat(2025): :white_check_mark: solve Gift Shop - Part 1

---
+++ 2025/gift-shop/index.ts
@@ -0,0 +1,24 @@
+import fs from "fs/promises";
+
+function isInvalidId(id: number) {
+  const stringId = id.toString();
+  const left = stringId.slice(0, stringId.length / 2);
+  const right = stringId.slice(stringId.length / 2);
+  return left === right;
+}
+
+function partOne(ranges: string[]) {
+  const invalidIds: number[] = [];
+  ranges.forEach((range) => {
+    const [start, end] = range.split("-").map(Number);
+    for (let id = start!; id <= end!; id++) {
+      if (isInvalidId(id)) {
+        invalidIds.push(id);
+      }
+    }
+  });
+  console.log(invalidIds.reduce((sum, id) => sum + id));
+}
+
+const lines = await fs.readFile("./input.txt", "utf-8");
+partOne(lines.split(","));
